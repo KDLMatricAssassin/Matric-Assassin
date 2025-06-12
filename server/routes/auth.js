893d8cf97e42cd8a16395f1d4e76ba34b6f2113d
MatricAssassin <yotam.limor3@gmail.com>
2025-06-12 02:42:03 +0200

    Fix auth route function names

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { login, register } = require('../controllers/authController');
+const { loginUser, registerUser } = require('../controllers/authController');
 
 // Authentication routes
-router.post('/login', login);
-router.post('/register', register);
+router.post('/login', loginUser);
+router.post('/register', registerUser);
 
 module.exports = router;
